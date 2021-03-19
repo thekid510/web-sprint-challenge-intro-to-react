5f33c09c8385808a958af3fa64dd18dbd88471c6
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-03-19 15:44:19 -0700

    added dates

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,22 +3,24 @@
 import React from 'react';
 import styled from "styled-components"
 const StyledTitle = styled.div`
-color:red;
-` 
+    color:red;
+    font-size:30px;
+    ` 
 const DataContainer = (props) => {
     const { swData } = props;
     console.log(props)
   return ( 
       <div> 
-          {swData && <img src ={swData.url} />}
-            {console.log(swData.url)}
+        
              {swData && <h1 title= {swData.name} />}
                  {console.log(swData.name)}
-                 <StyledTitle className ='name'>
-                    {swData.name}
         
-
+                <StyledTitle className ='name'>
+                    {swData.name}
                  </StyledTitle>
+                 {swData && <p title ={swData.birth_year} />}
+                     {console.log(swData.birth_year)}
+                     {swData.birth_year}
       </div>
   )  
 } 
