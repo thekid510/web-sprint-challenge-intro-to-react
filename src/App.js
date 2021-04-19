2c2a666340b64b6d30e048c40e934bd831eafe3c
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-18 19:18:14 -0700

    finished styled components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from "axios";
 import styled from 'styled-components'
-
-
+import DataContainer from "../src/components/Character"
+ 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,12 +12,13 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [starWarsData, setStarWarsData] = useState([])
+  const [swData, setSwData] = useState([])
+ 
     useEffect(()=>{
       axios
       .get("https://swapi.dev/api/people")
       .then(res => {
-        setStarWarsData(res.data)
+        setSwData(res.data)
         console.log(res.data);
       })
       .catch(err => {
@@ -25,18 +26,19 @@ const App = () => {
       })
     },[])
 const StyledHeader=styled.h1 `
-color: blue;
-transition: fill 0.25s;
-&hover{
-color: red;
-}
+  color: blue;
+  transition: fill 0.25s;
 
 `
-
-
   return (
     <div className="App">
-      <StyledHeader className="Header">Star Wars Episode 4 "A New Hope" Characters</StyledHeader>
+      <StyledHeader className="Header">Star Wars Episode 4 "A New Hope" Characters
+      </StyledHeader>
+      {swData.map(item => (
+        <DataContainer key={item.name} swData={item} swName={item} swBirth={item} />
+      ))
+
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+    color: red;
+    font-size: 20px;
+
+`
+const StyledAttributes = styled.div`
+    color: yellow;
+    font-size 20px;
+    padding:1.5%;
+`
+const DataContainer = (props) => {
+    const { swData } = props;
+    console.log (props)
+
+    return(
+        <div>
+        <StyledCharacter className="character" >
+            {swData.name}
+        </StyledCharacter>
+        <ol>
+        <StyledAttributes className="attrib">
+            {` Birth Year: ${swData.birth_year}`}
+            {`Height: ${swData.height}cm & Mass: ${swData.mass}kg`}
+            </StyledAttributes>
+            </ol>
+        </div>
+
+
+)
+}
+
+export default DataContainer;
